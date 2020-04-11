dddde02eb91becc470c0c62a2648558aeef08758
DolphinChips <DoIphinChips@protonmail.com>
2020-04-11 20:05:18 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+#!/system/bin/sh
+
 # Create working dir
 mkdir -p /sdcard/abm
 
@@ -5,7 +7,7 @@ mkdir -p /sdcard/abm
 dd if=/dev/block/bootdevice/by-name/boot of=/sdcard/abm/stockboot.img
 
 # Flash lk2nd to boot partition
-dd if=$1 of=/dev/block/bootdevice/by-name/boot
+dd if="$1" of=/dev/block/bootdevice/by-name/boot
 
 # Mount bootset
 mkdir -p /data/bootset
@@ -13,25 +15,29 @@ mount /dev/block/mmcblk0p51 /data/bootset
 
 # Create folder for current OS
 mkdir -p /data/bootset/lk2nd/entries
-mkdir -p /data/bootset/$2
+mkdir -p "/data/bootset/$2"
 
 # Copy device tree
-cp /sys/firmware/fdt /data/bootset/$2/dtb.dtb
+cp /sys/firmware/fdt "/data/bootset/$2/dtb.dtb"
 
 # Copy kernel
 mkdir -p /sdcard/abm/temp/boot
 unpackbootimg -i /sdcard/abm/stockboot.img -o /sdcard/abm/temp/boot > /dev/null 2>&1
-cp /sdcard/abm/temp/boot/stockboot.img-zImage /dcard/bootset/$2/zImage
+cp /sdcard/abm/temp/boot/stockboot.img-zImage "/sdcard/bootset/$2/zImage"
 
 # Copy rd
-cp /sdcard/abm/temp/boot/stockboot.img-ramdisk.gz /dcard/bootset/$2/inird.cpio.gz
+cp /sdcard/abm/temp/boot/stockboot.img-ramdisk.gz "/sdcard/bootset/$2/inird.cpio.gz"
 
-# Create entery
+# Create entry
 cmdline=$(cat /proc/cmdline)
-echo  "  default    Entry 01" >> /sdcard/bootset/lk2nd/lk2nd.conf
-echo  "  timeout    5" >> /sdcard/bootset/lk2nd/lk2nd.conf
-echo  "  title      $3" >> /sdcard/bootset/lk2nd/entries/entry01.conf
-echo  "  linux      $2/zImage" >> /sdcard/bootset/lk2nd/entries/entry01.conf 
-echo  "  initrd     $2/initrd.cpio.gz" >> /sdcard/bootset/lk2nd/entries/entry01.conf
-echo  "  dtb        $2/dtb.dtb" >> /sdcard/bootset/lk2nd/entries/entry01.conf
-echo  "  options    $cmdline  >> /sdcard/bootset/lk2nd/entries/entry01.conf
+cat << EOF >> /sdcard/bootset/lk2nd/lk2nd.conf
+   default    Entry 01
+   timeout    5
+EOF
+cat << EOF >> /sdcard/bootset/lk2nd/entries/entry01.conf
+  title      $3
+  linux      $2/zImage
+  initrd     $2/initrd.cpio.gz
+  dtb        $2/dtb.dtb
+  options    $cmdline
+EOF
