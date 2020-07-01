a61621d5d6199f9a2c1bdbac5d3ef35343c2ac68
luka177 <lukapanio@gmail.com>
2020-07-01 17:41:54 +0300

    Add board-id detection

--- add_rom_qcom.sh
+++ add_rom_qcom.sh
@@ -14,7 +14,7 @@ mount -t ext4 /dev/block/bootdevice/by-name/oem /data/bootset
 mkdir -p "/data/bootset/$2"
 
 #Unarchive it
-unzip $3 -d /sdcard/abm/tmp/rom
+unzip "$3" -d /sdcard/abm/tmp/rom
 
 #Copy boot
 cp /sdcard/abm/tmp/rom/boot.img /sdcard/abm/tmp/boot
@@ -31,6 +31,14 @@ lz4 -d /sdcard/abm/tmp/dt/dtlz4 /sdcard/abm/tmp/dt/dt
 #Go to dt dir, ectract dtb and go back
 cwd=$(pwd)
 cd /sdcard/abm/tmp/dt/
-$cwd/dtimgextract dt
-cd $cwd
+"$cwd/dtimgextract" dt
+cd "$cwd"
 
+#Get current rom DTB
+cp /sys/firmware/fdt /sdcard/abm/tmp/dt/current.dtb
+dtc -I dtb -O dts -o /sdcard/abm/tmp/dt/current.dts /sdcard/abm/tmp/dt/current.dtb
+
+#Get board id
+bid=$(cat /sdcard/abm/tmp/dt/current.dts | grep board-id)
+bid=$(echo "$bid" | awk '{print $4}')
+bid=${bid:2:4}
