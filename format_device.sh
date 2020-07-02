8e13ffa9c3c5e095b4dd094211067d6cc955aa7e
luka177 <lukapanio@gmail.com>
2020-07-02 17:30:28 +0300

    Add format_device.sh

---
+++ format_device.sh
@@ -0,0 +1,18 @@
+#!/system/bin/sh
+
+mkdir /data/abmmeta
+if [ "$1" = "usb" ]; then
+    sfdisk /dev/block/sda < partition_table.sfdisk
+    mkfs.ext4 /dev/block/sda1
+    mount /dev/block/sda1 /data/abmmeta
+elif [ "$1" = "sd" ]; then
+    sfdisk /dev/mmcblk1 < partition_table.sfdisk
+    mkfs.ext4 /dev/mmcblk1p1
+    mount /dev/mmcblk1p1 /data/abmmeta
+else
+    rm -r /data/abmmeta
+    exit 1
+fi
+
+touch /data/abmmeta/abm-drive.cfg
+umount /data/abmmeta
