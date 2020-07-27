7cc8d9aea4a8d50a8faa9976af27ccdd4333200b
nift4 <nift4@protonmail.com>
2020-07-27 20:19:30 +0200

    Prevent Android of doing an instant overwite

--- format_device.sh
+++ format_device.sh
@@ -2,12 +2,12 @@
 PATH=.:$PATH
 mkdir -p /data/abmmeta
 if [ "$1" = "usb" ]; then
-    sgdisk --clear --new=1:2048:32768 /dev/block/sda
+    sgdisk --clear --new=1:2048:32768 --typecode=1:ea00 /dev/block/sda
     partprobe; sleep 3
     true | mkfs.ext4 /dev/block/sda1
     mount /dev/block/sda1 /data/abmmeta
 elif [ "$1" = "sd" ]; then
-    sgdisk --clear --new=1:2048:32768 /dev/block/mmcblk1
+    sgdisk --clear --new=1:2048:32768 --typecode=1:ea00 /dev/block/mmcblk1
     partprobe; sleep 3
     true | mkfs.ext4 /dev/block/mmcblk1p1
     mount /dev/block/mmcblk1p1 /data/abmmeta
