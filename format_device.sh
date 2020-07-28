32d9eea8eb3efe61864af5f982d656d6cdca793e
nift4 <nift4@protonmail.com>
2020-07-28 08:07:26 +0200

    no typos allowed.

--- format_device.sh
+++ format_device.sh
@@ -8,7 +8,7 @@ if [ "$1" = "usb" ]; then
     mount /dev/block/sda1 /data/abmmeta
 elif [ "$1" = "sd" ]; then
     sgdisk --clear --new=1:2048:32768 --typecode=1:ea00 /dev/block/mmcblk1
-    pblockdev --rereadpt /dev/block/mmcblk1; sleep 3
+    blockdev --rereadpt /dev/block/mmcblk1; sleep 3
     true | mkfs.ext4 /dev/block/mmcblk1p1
     mount /dev/block/mmcblk1p1 /data/abmmeta
 else
