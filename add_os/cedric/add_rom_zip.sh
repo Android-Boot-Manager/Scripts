3152ba291315597acdbd6875f29ea6d67eb8fd92
nift4 <nift4@protonmail.com>
2020-07-29 09:25:46 +0200

    Update add_rom_zip.sh

--- add_os/cedric/add_rom_zip.sh
+++ add_os/cedric/add_rom_zip.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
 
-# Script for installing ROM zip, with halium's system image and halium boot for ABM. Parametrs: ROM folder name, ROM name in menu,
+# Script for installing ROM zip. Parameters: ROM folder name, ROM name in menu,
 # ROM zip, entry number.
 
 PATH=.:$PATH
@@ -62,6 +62,7 @@ echo $(($endofpart + 1+8388608)) > /data/abmmeta/endofparts
 umount /data/abmmeta
 
 #Write partition table
+#TODO: Create data
 sgdisk --new=$(($(ls /dev/block/mmcblk1p* | sed 's/ //g' | grep -Eo '[0-9]+$' ) + 1)):$(($endofpart + 1)):+8388608 /dev/block/mmcblk1
 
 partprobe /dev/block/mmcblk1; sleep 2
@@ -75,6 +76,7 @@ systempart=$(echo $(ls /dev/block/mmcblk1p*) | sed 's/ //g' | grep -Eo '[0-9]+$'
 true | mkfs.ext4 "/dev/block/mmcblk1p$systempart"
 
 #Patch dts
+#TODO: Change data
 sed -i "s/\/dev\/block\/platform\/soc\/7824900.sdhci\/by-name\/system/\/dev\/block\/platform\/soc\/7864900.sdhci\/mmcblk1p$systempart/g" /sdcard/abm/tmp/dtpatch/dtb.dts
 
 #Compile dts
@@ -85,6 +87,8 @@ cp /sdcard/abm/tmp/boot/boot.img-zImage "/data/bootset/$1/zImage"
 
 #Copy rd
 cp /sdcard/abm/tmp/boot/boot.img-ramdisk.gz "/data/bootset/$1/initrd.cpio.gz"
+
+#TODO: Flash system
 
 #Create entry
 cmdline=$(cat /sdcard/abm/tmp/boot/boot.img-cmdline)
