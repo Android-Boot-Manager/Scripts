f3ffc6de82fde5001d0e1d6c25cbbc320c5b13a5
nift4 <nift4@protonmail.com>
2020-08-20 13:52:37 +0200

    Update add_ubuntutouch_sytemimage_haliumboot_rootfs.sh

--- add_os/META-INF/add_ubuntutouch_sytemimage_haliumboot_rootfs.sh
+++ add_os/META-INF/add_ubuntutouch_sytemimage_haliumboot_rootfs.sh
@@ -2,7 +2,8 @@ native getString str_installing_title installing_title
 dialoginfo %str_installing_title% 'No, not really. This is just an ABM MetaScript demo.'
 dialogchoice Demo Continue?
 dialogloading Demo 'Please wait...'
-exec 'sh -c "sleep 3; echo magic"' output
+exec 'sleep 3' _
+exec 'echo magic' output
 dialogloadingquit
 dialoginfo Demo 'exec gave %output%'
 dialogfile file
