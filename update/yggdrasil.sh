94506cc21c6f1c8ccddff55699361865c6155bbe
luka177 <lukapanio@gmail.com>
2020-08-20 10:02:30 +0300

    Create yggdrassil update seipt

---
+++ update/yggdrasil.sh
@@ -0,0 +1,8 @@
+#!/system/bin/sh
+
+# Backup
+dd if=/dev/block/by-name/lk  of=/sdcard/abm/stocklk.img
+
+# Flash lkbootloader to lk partition
+dd if=/data/data/org.androidbootmanager.app/files/lk2nd.img of=/dev/block/by-name/lk 
+
