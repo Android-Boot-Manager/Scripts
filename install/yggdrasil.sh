823aada97613b4e4f7aad69d88b5b479bf982292
nift4 <nift4@protonmail.com>
2020-12-07 16:55:27 +0100

    Update yggdrasil.sh

--- install/yggdrasil.sh
+++ install/yggdrasil.sh
@@ -4,12 +4,12 @@
 mkdir -p /sdcard/abm
 
 # Backup
-dd if=/dev/block/by-name/lk  of=/sdcard/abm/stocklk.img
+dd if=/dev/block/by-name/lk of=/sdcard/abm/stocklk.img
 
 # Flash lbootloader to lk partition
 dd if="$1" of=/dev/block/by-name/lk 
 
-#Kill logcat that opens some magisk log in cache
+# Kill logcat that opens some magisk log in cache
 pkill -f logcat
 
 # Format cache
@@ -19,21 +19,21 @@ dd if=vollacache.img of=/dev/block/by-name/cache
 rm vollacache.img
 
 # Mount cache
-mkdir -p /data/bootset/lk2nd
 mount -t ext4 /dev/block/by-name/cache /cache
-mount --bind /cache /data/bootset
-mkdir -p /data/bootset/db
-mount --bind /data/bootset/db /data/bootset/lk2nd
+mount --bind /cache /data/abm/bootset
+mkdir -p /data/abm/bootset/db
+mkdir -p /data/abm/bootset/lk2nd
+mount --bind /data/abm/bootset/db /data/abm/bootset/lk2nd
 
 # Create folder for entries
-mkdir -p /cache/db/entries
+mkdir -p /data/abm/bootset/db/entries
 
 # Create entry
-cat << EOF >> /cache/db/db.conf
+cat << EOF >> /data/abm/bootset/db/db.conf
    default    Entry 01
    timeout    5
 EOF
-cat << EOF >> /cache/db/entries/entry01.conf
+cat << EOF >> /data/abm/bootset/db/entries/entry01.conf
   title      $3
   linux      null
   initrd     null
@@ -44,13 +44,4 @@ cat << EOF >> /cache/db/entries/entry01.conf
   xRomData   real
 EOF
 
-cat > /data/abm-part.cfg << EOF
-mkdir -p /data/bootset/lk2nd
-mount -t ext4 /dev/block/by-name/cache /cache
-mount --bind /cache /data/bootset
-mount --bind /data/bootset/db /data/bootset/lk2nd
-EOF
-
-cat > /data/abm-part.2.cfg << EOF
-# useless.
-EOF
+cat yggdrasil > /data/abm/codename.cfg
