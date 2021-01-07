5133e59f83aac4adbf9fa003e83569493c06b514
nift4 <nift4@protonmail.com>
2021-01-07 16:21:01 +0100

    Update yggdrasil.droid.sh

--- update/yggdrasil.droid.sh
+++ update/yggdrasil.droid.sh
@@ -1,2 +1,10 @@
 # Flash lkbootloader to lk partition
 dd if=/data/data/org.androidbootmanager.app/files/lk2nd.img of=/dev/block/by-name/lk 
+
+echo "===== INFORMATION ======"
+echo "This feature often does not"
+echo "work for unknown reasons"
+echo "on this device. This is not"
+echo "harmful, just nothing will"
+echo "happen. You can flash the"
+echo "update via fastboot flash lk"
