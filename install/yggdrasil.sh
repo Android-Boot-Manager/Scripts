d3d581009c840948d7e97b3aab6d08615027a239
nift4 <nift4@protonmail.com>
2021-02-02 21:26:12 +0100

    Do not format /cache
    This causes an shit ton of bugs. Just let TWRP handle this, it does this job safely.

--- install/yggdrasil.sh
+++ install/yggdrasil.sh
@@ -9,18 +9,6 @@ dd if=/dev/block/by-name/lk of=/sdcard/abm/stocklk.img
 # Flash lbootloader to lk partition
 dd if="$1" of=/dev/block/by-name/lk 
 
-# Kill logcat that opens some magisk log in cache
-pkill logcat
-pkill -f logcat
-
-# Format cache
-umount /cache || umount -f /cache
-./lz4 -d vollacache.img.lz4 vollacache.img
-dd if=vollacache.img of=/dev/block/by-name/cache
-rm vollacache.img
-
-# Mount cache
-mount -t ext2 /dev/block/by-name/cache /cache
 mkdir -p /cache/db /cache/lk2nd
 mount --bind /cache /data/abm/bootset
 mount --bind /data/abm/bootset/db /data/abm/bootset/lk2nd
