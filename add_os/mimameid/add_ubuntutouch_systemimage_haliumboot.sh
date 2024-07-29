36afa4bce9bd65b4c76cccf56a951fbaeec52372
nift4 <nift4@protonmail.com>
2024-07-29 11:29:34 +0200

    update install scripts for mima

--- add_os/mimameid/add_ubuntutouch_systemimage_haliumboot.sh
+++ add_os/mimameid/add_ubuntutouch_systemimage_haliumboot.sh
@@ -4,7 +4,11 @@ TK="/data/data/org.andbootmgr.app/assets/Toolkit"
 PATH="$TK:$PATH"
 cd "$TK" || exit 24
 
-sed -i "s#replaceme#systempart=/dev/mmcblk1p$3 datapart=/dev/mmcblk1p$4#g" /data/abm/bootset/db/entries/"$1".conf
+sed -i "s#replaceme#systempart=/dev/mmcblk1p$4 datapart=/dev/mmcblk1p$5#g" /data/abm/bootset/db/entries/"$1".conf
+
+# Add logo
+echo "logo $1/logo.bin" >> /data/abm/bootset/db/entries/"$1".conf
+cp "$3" "/data/abm/bootset/$1/logo.bin"
 
 # Create working dir
 mkdir -p /data/abm/tmp/boot
@@ -14,6 +18,14 @@ cp "$2" /data/abm/tmp/boot/boot.img
 
 # Unpack boot
 unpackbootimg -i /data/abm/tmp/boot/boot.img -o /data/abm/tmp/boot/
+
+# Format partition
+DATAPART=$5
+dataformat() {
+true | mkfs.ext4 "/dev/block/mmcblk1p$DATAPART"
+}
+
+$FORMATDATA && dataformat
 
 # Copy kernel
 cp /data/abm/tmp/boot/boot.img-zImage "/data/abm/bootset/$1/zImage"
